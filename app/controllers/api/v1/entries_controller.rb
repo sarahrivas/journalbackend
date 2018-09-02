7bb1cb6a46a20b7e44b02893370f9750237e9166
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-09-02 16:34:11 -0400

    updated entry controller

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,17 +1,40 @@
 class Api::V1::EntriesController < ApplicationController
+  before_action :set_entry, only: [:show,:update,:destroy]
 
   def index
     @entries = Entry.all.with_attached_images
+    render json: entries, status: 200
   end
 
   def create
     entry = Entry.create! params.require(:entry).permit(:title, :content)
     entry.images.attach(params[:entry][:images])
     redirect_to message
+    render json: entry, status: 201
+  end
+
+  def update
+    @entry.update(entry_params)
+    render json: @entry, status: 200
   end
 
   def show
-    @entry = Entry.find(param[:id])
+    render json: @entry, status: 200
   end
-  
+
+  def destroy
+    entryId = @entry.id
+    @entry.destroy
+    render json: {message: "Your journal entry has been deleted!", entryId:entryId}
+  end
+
+private
+    def entry_params
+      params.permit(:title, :content, :user_id)
+    end
+
+    def set_entry
+      @entry = Entry.find(params[:id])
+    end
+
 end
