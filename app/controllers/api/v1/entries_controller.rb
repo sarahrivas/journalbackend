d9c451a73be4eae5681c1dbd7a61b629764dbfa4
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-09-02 16:08:21 -0400

    active storage setup done

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::EntriesController < ApplicationController
+
+  def index
+    @entries = Entry.all.with_attached_images
+  end
+
+  def create
+    entry = Entry.create! params.require(:entry).permit(:title, :content)
+    entry.images.attach(params[:entry][:images])
+    redirect_to message
+  end
+
+  def show
+    @entry = Entry.find(param[:id])
+  end
+  
 end
