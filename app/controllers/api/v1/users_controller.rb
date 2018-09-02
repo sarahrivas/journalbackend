30623a8c54c76e17d352dd56ec3cf27a4e82ee5d
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-09-02 16:53:34 -0400

    set up user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 skip_before_action :authorized, only: i%[create]
+before_action :set_user, only: [:show, :update, :destroy]
 
   def profile
       logged_in ? (render json: { user: UserSerializer.new(current_user) }, status: :accepted) : (render json:{ message: 'User not found' }, status: :not_found})
@@ -15,10 +16,34 @@ skip_before_action :authorized, only: i%[create]
     end
   end
 
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+
+  def update
+    @user.update(user_params)
+    render json: @user, status: 200
+  end
+
+  def show
+    render json: @user, status: 200
+  end
+
+  def destroy
+    userId = @user.id
+    @user.destroy
+    render json: {message:"User has been deleted.", userId:userId}
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
